41a73914a036f53f3a629842b28dc3c55a39fe97
Timotej Trampuz <timotej.trampuz@tscng.org>
2025-10-03 11:27:20 +0200

    Add files via upload
    Dodani Hanoi in Palindromi

---
+++ HanoiTower/HanoiTower/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HanoiTower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi(5, "a", "c", "b");
+        }
+
+        static void Hanoi(int n, string zač, string končni, string pomoč)
+        {
+            if (n == 1) { 
+                Console.WriteLine("premakni obroč iz "+zač+" na "+končni);
+                return;
+            }
+
+            Hanoi(n - 1, zač, pomoč, končni);
+            Console.WriteLine("premakni obroč iz " + zač + " na " + končni);
+            Hanoi(n - 1, pomoč, končni, zač);
+        }
+    }
+}
---
+++ Palindromi/Palindromi/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Palindromi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "kajak";
+            string b = "mark";
+            string c = "pericarežeracirep";
+
+            JePalindrom(a, a);
+            JePalindrom(b, b);
+            JePalindrom(c, c);
+        }
+
+        static void JePalindrom(string n, string og)
+        {
+          if (n.Length == 0 || n.Length == 1)
+                Console.WriteLine("Beseda " + og + " je palindrom. ;)");
+            else if (n[0] == n[n.Length - 1])
+                JePalindrom(n.Substring(1, n.Length - 2), og);
+            else
+                Console.WriteLine("Beseda " + og + " ni palindrom. :(");
+        }
+    }
+}
