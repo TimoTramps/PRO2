7ab482b2226e729a1b46f2937621e31188c75a04
Timotej Trampuz <timotej.trampuz@tscng.org>
2025-10-03 10:21:48 +0200

    Reči za PRO
    mape in programi ;)

---
+++ Rekurzija/Rekurzija/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Http.Headers;
+
+namespace Rekurzija
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a = fakulteta(5);
+            Console.WriteLine(a);
+
+            int b = fr(5);
+            Console.WriteLine(b);
+
+            int c = Fibonacci(7);
+            Console.WriteLine(c);
+
+            int d = recf(5);
+            Console.WriteLine(d);
+        }
+
+        static int fakulteta(int n)
+        {
+            int produkt = 1;
+            for (int k = n; k > 1; k--)
+                produkt *= k;
+            return produkt;
+
+        }
+
+        //rekurzvno
+
+        static int fr(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * fr(n - 1);
+        }
+
+        //fibonacci
+
+        static int Fibonacci(int n)
+        {
+
+            if (n == 1 || n == 2)
+                return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        //2,2,3,5,14,69,965
+
+        //a1=2, a2=2, a(n) = a(n-2)*a(n-1)-1
+
+
+        static int recf(int n)
+        {
+            if (n == 1 || n == 2)
+                return 2;
+            return recf(n - 2) * recf(n - 1) - 1;
+        }
+    }
+}
---
+++ vaja_zanke/vaja_zanke/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace vaja_zanke
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Naloga 1
+            int stevec = 13;
+            if (stevec > 10)
+                Console.WriteLine("Števec je večji od 10");
+
+            // Naloga 2
+            int sum = 0;
+            for (int k = 1; k <= 99; k = k + 2)
+                sum += k;
+            Console.WriteLine(sum);
+
+            // Naloga 3
+            int sum1 = 0;
+            int k1 = 1;
+            do{
+                sum1 += k1;
+                k1 = k1 + 2;
+            } while (k1 <= 99);
+            Console.WriteLine(sum1);
+
+
+            Console.WriteLine("");
+            //Naloga 4
+            int j = 1;
+            while (j <= 20)
+            {
+                Console.Write("{0,4}", j);
+                if(j%5==0)
+                    Console.WriteLine();
+                j++;
+            }
+        }
+    }
+}
