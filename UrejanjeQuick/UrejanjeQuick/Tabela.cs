8c376ca111f20abe54d7160ca0c1d047b2f436c9
Timotej Trampuz <timotej.trampuz@tscng.org>
2025-10-03 11:43:18 +0200

    Add files via upload

---
+++ UrejanjeQuick/UrejanjeQuick/Tabela.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrejanjeQuick
+{
+    internal class Tabela
+    {
+        int[] tab;
+        int štEementov;
+
+        public Tabela(int velikost)
+        {
+            tab = new int[velikost];
+            štEementov = 0;
+        }
+        public void Dodaj(int k)
+        {
+            tab[štEementov++] = k;
+        }
+        public void Izpisi()
+        {
+            for (int k = 0; k < tab.Length; k++) 
+            {
+                Console.Write("{0,4}", tab[k]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
