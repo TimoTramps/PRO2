d24099831203e70b433d7c9bd6c9bb6999ce3122
Timotej Trampuz <timotej.trampuz@tscng.org>
2025-10-09 12:17:02 +0200

    Update Fraktali

--- Fraktali/Fraktali/Form1.cs
+++ Fraktali/Fraktali/Form1.cs
@@ -33,6 +33,42 @@ namespace Fraktali
             Pen p = new Pen(Color.Black);
             g.DrawLine(p, UmeriX(xstar), UmeriY(ystar), UmeriX(x), UmeriY(y));
         }
+        public void Koch(int n, double korak, Graphics g)
+        {
+            if (n == 0)
+            {
+                Premik(korak, g);
+                return;
+            }
+            Koch(n - 1, korak, g);
+            ObratLevo(60);
+            Koch(n - 1, korak, g);
+            ObratLevo(-120);
+            Koch(n - 1, korak, g);
+            ObratLevo(60);
+            Koch(n - 1, korak, g);
+        }
+
+        
+
+        public void FraktalnoDrevo(int n, double x, double y, double a, double korak, Graphics g)
+        {
+            double cx = x + Math.Cos(a * Math.PI / 180) * korak;
+            double cy = y + Math.Sin(a * Math.PI / 180) * korak;
+            g.DrawLine(new Pen(Color.Black), UmeriX(x), UmeriY(y), UmeriX(cx), UmeriY(cy));
+
+            if (n == 0)
+            {
+                //Premik(korak, g);
+                return;
+            }
+
+            FraktalnoDrevo(n - 1, cx, cy, a + 45, korak * 0.65, g);
+            FraktalnoDrevo(n - 1, cx, cy, a, korak * (1-0.65), g);
+            FraktalnoDrevo(n - 1, cx, cy, a - 45, korak * 0.65, g);
+        }
+
+
 
         public Form1()
         {
@@ -43,19 +79,24 @@ namespace Fraktali
         {
             Graphics g = e.Graphics;
 
-            x = 0.3; y = 0; a = 30;
+            //KOCH
+            //x = 0.2; y = 0.7;a = 0;
+            //int n = 6;
+            //double korak = 1 / Math.Pow(3.25, n);
+            //Koch(n, korak, g);
 
-            Premik(0.3, g);
-            ObratLevo(60);
-            Premik(0.3, g);
-            ObratLevo(60);
-            Premik(0.3, g);
-            ObratLevo(60);
-            Premik(0.3, g);
-            ObratLevo(60);
-            Premik(0.3, g);
-            ObratLevo(60);
-            Premik(0.3, g);
+            //a = -120;
+            //Koch(n, korak, g);
+            //a = -240;
+            //Koch(n, korak, g);
+
+            //FRAKTALNO DREVO
+            x = 0.5;y = 0;
+            a = 90;
+
+            int n = 10; // globina rekurzije, veèja = bolj kompleksno drevo
+            double korak = 1 / Math.Pow(1.15, n);
+            FraktalnoDrevo(n, x, y, a, korak, g);
         }
     }
 }
