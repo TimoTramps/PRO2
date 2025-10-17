e80b596a3fcbea697077360e930685a6cc254118
Timotej Trampuz <timotej.trampuz@tscng.org>
2025-10-17 11:48:06 +0200

    Preobtežitev

---
+++ Preobtez/Preobtez/Pravokotnik.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preobtez
+{
+    internal class Pravokotnik
+    {
+        public double Širina { get; set; }
+        public double Dolžina { get; set; }
+
+        public Pravokotnik()
+        {
+            Širina = 0;
+            Dolžina = 0;
+        }
+
+        public Pravokotnik(double sirina, double dolzina)
+        {
+            sirina = Širina;
+            dolzina = Dolžina;
+        }
+
+        public double Ploščina()
+        {
+            return Širina * Dolžina;
+        }
+        public string ToString()
+        {
+            return "širina: " + Širina + ", dolžina: " + Dolžina;
+        }
+    }
+}
---
+++ Preobtez/Preobtez/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace Preobtez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pravokotnik p1 = new Pravokotnik();
+            Pravokotnik p2 = new Pravokotnik();
+            Pravokotnik p3 = new Pravokotnik(10, 15);
+            p1.Širina = 10;
+            p1.Dolžina = 15;
+            p2.Širina = 25; p2.Dolžina = 10;
+            Console.WriteLine("Pravokotnik 1 " + p1.ToString());
+            Console.WriteLine("Pravokotnik 1 " + p2.ToString());
+            Console.WriteLine("Pravokotnik 1 " + p3.ToString());
+            if (p1.Ploščina() == p2.Ploščina()) Console.WriteLine("p1=p2");
+            else
+            {
+                if (p1.Ploščina() > p2.Ploščina()) Console.WriteLine("P1 je večji od P2");
+                else Console.WriteLine("P2 je večji od P1");
+            }
+            if (p1.Ploščina() == p3.Ploščina()) Console.WriteLine("P1 je enak P3");
+            Console.ReadLine();
+        }
+    }
+}
