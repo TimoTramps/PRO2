6e864c9f4655b910ab99f1d3ca991c43265158aa
Timotej Trampuz <timotej.trampuz@tscng.org>
2025-11-17 11:46:58 +0100

    Add files via upload

---
+++ VajaJSON/VajaJSON/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace VajaJSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ucilnica
+            FileStream fs = new FileStream("Ucilnica.json", FileMode.Open);
+            Class1[] xs = JsonConvert.DeserializeObject<Class1[]>(File.ReadAllText(@"Ucilnica.json"));
+
+            for(int k=0; k <= xs.Length; k++) { 
+
+            foreach (Module dm in xs[k].modules)
+            {
+                    //Console.WriteLine(dm[k].name);
+            }
+            
+            }
+        }
+    }
+}
---
+++ VajaXML/VajaXML/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+
+namespace VajaXML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Racuni
+            FileStream fs = new FileStream("Racuni.xml", FileMode.Open);
+            XmlSerializer xs = new XmlSerializer(typeof(IzdaniRacunEnostavni));
+            IzdaniRacunEnostavni d = (IzdaniRacunEnostavni)xs.Deserialize(fs);
+
+            foreach (IzdaniRacunEnostavniRacunPostavkeRacuna dm in d.Racun.PostavkeRacuna)
+            {
+                Console.WriteLine(dm.OpisiArtiklov.OpisArtikla.OpisArtikla1 + " " + dm.KolicinaArtikla.Kolicina + " " + dm.ZneskiPostavke.ZnesekPostavke);
+            }
+        }
+    }
+}
